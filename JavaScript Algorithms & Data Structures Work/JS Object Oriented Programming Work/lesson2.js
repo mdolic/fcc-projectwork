8eceebb8690d9814fde94d4695978e07d238dc43
mdolic <mdolic1@dmacc.edu>
2018-07-06 07:55:12 -0500

    updated

--- JavaScript Algorithms & Data Structures Work/JS Object Oriented Programming Work/lesson2.js
+++ JavaScript Algorithms & Data Structures Work/JS Object Oriented Programming Work/lesson2.js
@@ -8,7 +8,8 @@ Bird.prototype = Object.create(Animal.prototype);
 Dog.prototype = Object.create(Animal.prototype);
 
 // Add your code below this line
-
+Bird.prototype.constructor = Bird; //manually changed the bird constructor property to the bird object
+Dog.prototype.constructor = Dog;//manually changed the bird constructor property to the dog object
 
 
 let duck = new Bird();
