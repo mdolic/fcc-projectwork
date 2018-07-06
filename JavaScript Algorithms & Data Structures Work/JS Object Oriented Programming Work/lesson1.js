f093cdaddbf8f3c151a0a5eb244d8fd09f13cc33
mdolic <mdolic1@dmacc.edu>
2018-07-06 07:12:04 -0500

    updated

--- JavaScript Algorithms & Data Structures Work/JS Object Oriented Programming Work/lesson1.js
+++ JavaScript Algorithms & Data Structures Work/JS Object Oriented Programming Work/lesson1.js
@@ -153,8 +153,23 @@
                         console.log(` my name is ${this.name}`);
                     }
                   };
-
-        
+                  
+//how to Remember to Set the Constructor Property when Changing the Prototype
+                function Dog(name) {
+                    this.name = name; 
+                }
+                
+                // Modify the code below this line
+                Dog.prototype = {
+                    constructor: Dog,
+                    numLegs: 2, 
+                    eat: function() {
+                    console.log("nom nom nom"); 
+                    }, 
+                    describe: function() {
+                    console.log("My name is " + this.name); 
+                    }
+                };
 
 
 
