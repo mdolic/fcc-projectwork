69405c4f6764d99b1302969d0da3fc16eeeeb25e
mdolic <mdolic1@dmacc.edu>
2018-07-08 07:36:40 -0500

    updated

--- JavaScript Algorithms & Data Structures Work/JS Functional Programming work/lesson 1.js
+++ JavaScript Algorithms & Data Structures Work/JS Functional Programming work/lesson 1.js
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 
-<script>
+
+
 //lesson 1 learned about functional programming
 /**
  * A long process to prepare tea.
@@ -316,11 +308,3 @@ var watchList = [
 
 
 
-</script>
-
-
-
-
-
-</body>
-</html>
