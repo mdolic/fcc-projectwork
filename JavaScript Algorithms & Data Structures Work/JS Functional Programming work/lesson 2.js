bb79e629f67c9656d45b5ffba8ce0d144ba8cee7
mdolic <mdolic1@dmacc.edu>
2018-07-08 17:08:54 -0500

    added for loop code to recreate filter() method

--- JavaScript Algorithms & Data Structures Work/JS Functional Programming work/lesson 2.js
+++ JavaScript Algorithms & Data Structures Work/JS Functional Programming work/lesson 2.js
@@ -151,3 +151,21 @@ const filteredList  = watchList.filter((item)=>{
 console.log(filteredList); 
 
 //how to implement filter method on a prototype
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback){
+  var newArray = [];
+  // Add your code below this line
+    for(let i = 0;i<this.length;i++){
+      if(callback(i) === true)
+        newArray.push(this[i]);
+    }
+  // Add your code above this line
+  return newArray;
+
+};
+
+var new_s = s.myFilter(function(item){
+  return item % 2 === 1;
+});
