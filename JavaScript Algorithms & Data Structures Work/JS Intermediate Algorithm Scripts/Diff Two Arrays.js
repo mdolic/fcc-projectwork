5724338505d9819c48a0a2eb5ec1dee49ec4bea9
mdolic <mdolic1@dmacc.edu>
2018-07-12 21:15:31 -0500

    finished with notes

--- JavaScript Algorithms & Data Structures Work/JS Intermediate Algorithm Scripts/Diff Two Arrays.js
+++ JavaScript Algorithms & Data Structures Work/JS Intermediate Algorithm Scripts/Diff Two Arrays.js
@@ -9,9 +9,32 @@ You can return the array with its elements in any order.
 */
 
 function diffArray(arr1, arr2) {
-    var newArr = [];
+    var newArr = [...arr1,...arr2]; //spread arr1,arr2 into newArr
     // Same, same; but different.
-    return newArr;
+    let filteredArr = newArr.filter((elem)=>{ //filter on individual elemn
+      return newArr.indexOf(elem) == newArr.lastIndexOf(elem); //checks the indexOf(elem)
+    });
+   return filteredArr;
   }
   
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+  //alternative solution 2:
+
+  function diffArray(arr1,arr2){
+    return arr1
+      .concat(arr2) //joins arr2 to arr 1
+      .filter(item => !arr1.includes(item)|| !arr2.includes(item)) //items not included in arr1 & arr2
+  }
+
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+  //alternative solution 3:
+
+  function diffArray(arr1,arr2){
+    let filterArr1 = arr1.filter((item) => arr2.indexOf(item) ===-1);
+    let filterArr2 = arr2.filter((item)=> arr1.indexOf(item) ===-1);
+    return filterArr1.concat(filterArr2); 
+  }
+
   diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
