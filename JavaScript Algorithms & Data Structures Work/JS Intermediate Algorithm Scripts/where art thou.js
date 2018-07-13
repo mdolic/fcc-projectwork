cc8feb4e5d0376c4b08120532212a2d1b702269f
cashyus <mdolic1@dmacc.edu>
2018-07-13 14:53:24 -0500

    work in progress on this solution.. stuck

---
+++ JavaScript Algorithms & Data Structures Work/JS Intermediate Algorithm Scripts/where art thou.js
@@ -0,0 +1,17 @@
+function whatIsInAName(collection, source) {
+    // What's in a name?
+    var arr = [];
+    // Only change code below this line
+    for(let i = 0;collection.length;i++){
+        if(collection.hasOwnProperty(source) === 1){
+
+        }
+    }
+
+
+    // Only change code above this line
+    return arr;
+  }
+
+  whatIsInAName([{ first: "Romeo", last: "Montague" },
+  { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
