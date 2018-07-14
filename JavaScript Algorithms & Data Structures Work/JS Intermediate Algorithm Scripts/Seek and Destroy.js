825a4ee4b172bf4b103e02771e9236fede34bbb7
mdolic <mdolic1@dmacc.edu>
2018-07-14 06:12:33 -0500

    updated

--- JavaScript Algorithms & Data Structures Work/JS Intermediate Algorithm Scripts/Seek and Destroy.js
+++ JavaScript Algorithms & Data Structures Work/JS Intermediate Algorithm Scripts/Seek and Destroy.js
@@ -1,8 +1,11 @@
+
+
+//Remove all elements from the initial array that are of the same value as these arguments.
 function destroyer(arr) {
     // Remove all the values
     let args= Array.from(arguments);//takes in the arguments, places in the array
     return arr.filter((val)=>{ //goes through the array
-        return !args.includes(val);
+        return !args.includes(val); //does not include val
     });
   }
 
--- JavaScript Algorithms & Data Structures Work/JS Intermediate Algorithm Scripts/where art thou.js
+++ JavaScript Algorithms & Data Structures Work/JS Intermediate Algorithm Scripts/where art thou.js
@@ -1,12 +1,10 @@
+
+
 function whatIsInAName(collection, source) {
     // What's in a name?
     var arr = [];
     // Only change code below this line
-    for(let i = 0;collection.length;i++){
-        if(collection.hasOwnProperty(source) === 1){
-
-        }
-    }
+   let newObj = Object.keys(source); 
 
 
     // Only change code above this line
