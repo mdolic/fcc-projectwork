92f20509cf8cbeec943ad225fede9aca0b650244
mdolic <mdolic1@dmacc.edu>
2018-07-15 06:53:15 -0500

    final solution implemented

--- JavaScript Algorithms & Data Structures Work/JS Intermediate Algorithm Scripts/where art thou.js
+++ JavaScript Algorithms & Data Structures Work/JS Intermediate Algorithm Scripts/where art thou.js
@@ -2,13 +2,19 @@
 
 function whatIsInAName(collection, source) {
     // What's in a name?
-    var arr = [];
+    //var arr = [];
     // Only change code below this line
-   let newObj = Object.keys(source); 
+   let srcKeys = Object.keys(source); //srckeys = ["last"] , source = {last: "capulet"}
 
+   return  collection.filter((collectionItem)=>{ //filter through the collection array
+     return srcKeys.every((keyInObject) =>{ //use srckeys and find the keyInObject = ["last"]
+       return collectionItem[keyInObject] === source[keyInObject]; 
+              //["last "] === ["last"]
+     });
+   });
 
     // Only change code above this line
-    return arr;
+    
   }
 
   whatIsInAName([{ first: "Romeo", last: "Montague" },
