30d18038f8e3be32bf078d75cc9d3536dd86786e
mdolic <mirzetdolic2@gmail.com>
2018-12-26 10:35:39 -0600

    basic solution done

---
+++ JavaScript Algorithms & Data Structures Work/JS Intermediate Algorithm Scripts/search&replace.js
@@ -0,0 +1,21 @@
+function myReplace(str, before, after) {
+    let a = str.indexOf(before); //1
+    if(str[a] === str[a].toUpperCase()){ //2
+        after = after.charAt(0).toUpperCase() + after.slice(1); //3
+    }
+    str = str.replace(before,after); //4
+    return str; //5
+    }
+
+    /*
+        1. using indexOf() finds the location of the before string
+        2. check if first letter of before is captialized agaisnt first letter of
+        after. if not do step 3
+        3. change after first character to upper case
+        4. replace, before with after
+        5. finally return the string
+
+         links on mdn => indexOf, touppercase, charAt, slice
+        */
+
+    myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
