706f8962cf53638474e61f151ffeb7765f5da390
mdolic <mirzetdolic2@gmail.com>
2019-01-03 12:33:20 -0600

    refactored

--- JavaScript Algorithms & Data Structures Work/JS Intermediate Algorithm Scripts/dnapair.js
+++ JavaScript Algorithms & Data Structures Work/JS Intermediate Algorithm Scripts/dnapair.js
@@ -1,14 +1,19 @@
 function pairElement(str) {
   let emptyArr = [];
-  let a = ['G','K']
+  let a = ['G','C'];
+  let b = ['T','A'];
   let newArr = str.split("");
 
   newArr.map(item =>{
-    if(newArr.item[0] === 'A'){
+    if(item[0] === 'C'){
     emptyArr.push(a);
+    } else if(item[0]=== 'A'){
+      emptyArr.push(b);
+    } else if(item[0] === 'G'){
+      emptyArr.push(a);
     }
   });
   return emptyArr;
 
 }
-  pairElement("GCG");
+pairElement("ATCGA");
